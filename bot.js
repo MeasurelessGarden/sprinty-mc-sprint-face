b75ad63dab4862e5e0efdc60e00b8b938e3deaf8
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-03 21:25:40 -0600

    Cleanup test yml.

--- bot.js
+++ bot.js
@@ -1,25 +1,23 @@
-const Discord = require("discord.js");
-
-var auth = require('./secret.json');
+const Discord = require("discord.js")
+var auth = require('./secret.json')
 var yaml = require('js-yaml')
 var fs = require('fs')
-let test = yaml.load(fs.readFileSync('./test.yml', {encoding: 'utf-8'}))//require('./test.yml')
-console.log(test)
+let responses = yaml.load(fs.readFileSync('./responses.yml', {encoding: 'utf-8'}))
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("I am ready!")
 });
 
 client.on("message", (message) => {
   // console.log(message)
   if (message.author.bot) {return;}
   if (message.content.startsWith("ping")) {
-    message.channel.send("pong!\nfooo");
+    message.channel.send("pong!\nfooo")
   }
   if (message.content.startsWith("help")) {
-    message.channel.send(test.cat);
+    message.channel.send(responses.helpMessage)
   }
 });
 
