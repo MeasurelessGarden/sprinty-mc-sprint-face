ef424baad635402a174c98ad300626049d20d805
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-03 22:43:52 -0600

    Can DM sprinty with commands also.

--- bot.js
+++ bot.js
@@ -16,16 +16,28 @@ client.on("message", (message) => {
   if (message.author.bot) {return} // prevent botception
 
   _.each(config.commands, (command) => {
-    if(command.mentionRequired && _.find(message.mentions.users.array(), (userMention) => {
-      return userMention.id == auth.clientId
-    })) {
-      const msg = _.lowerCase(_.trim(_.replace(message.content, config.mentionMe, '')))
-      if (_.find(command.cmds, (cmd)=> cmd)) {
-        // console.log('found a command: ', command)
-        message.channel.send(command.response)
+    if(command.mentionRequired) {
+      if( message.channel.type == 'dm') {
+        console.log(message.channel)
+        console.log(message.content)
+        const msg = _.lowerCase(_.trim(_.replace(message.content, config.mentionMe, '')))
+        if (_.find(command.cmds, (cmd)=> {return cmd == msg })) {
+          message.author.send(command.response)
+        }
       }
+      else if( _.find(message.mentions.users.array(), (userMention) => {
+        return userMention.id == auth.clientId
+      })) {
+        const msg = _.lowerCase(_.trim(_.replace(message.content, config.mentionMe, '')))
+        if (_.find(command.cmds, (cmd)=> {return cmd == msg })) {
+          // console.log('found a command: ', command)
+          // message.channel.send(command.response)
+          message.author.send(command.response)
+        }
+    }
     }
   })
+
 });
 
 client.login(auth.token);
