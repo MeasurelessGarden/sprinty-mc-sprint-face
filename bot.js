0b856e9207aa532632a48fcec562826113a76c10
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-03 22:00:07 -0600

    Talk to sprinty directly for help, plus configuration of various options.

--- bot.js
+++ bot.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js")
 var auth = require('./secret.json')
 var yaml = require('js-yaml')
 var fs = require('fs')
-let responses = yaml.load(fs.readFileSync('./responses.yml', {encoding: 'utf-8'}))
+var _ = require('lodash')
 
+let config = yaml.load(fs.readFileSync('./config.yml', {encoding: 'utf-8'}))
 const client = new Discord.Client()
 
 client.on("ready", () => {
@@ -12,13 +13,22 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
   // console.log(message)
-  if (message.author.bot) {return;}
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!\nfooo")
-  }
-  if (message.content.startsWith("help")) {
-    message.channel.send(responses.helpMessage)
-  }
+  if (message.author.bot) {return} // prevent botception
+
+  // if (_.has(message.mentions.users, auth.clientId)) {
+  //   console.log("You are talking to me")
+  // } else {
+  //   console.log("you're not talking to me")
+  //   // console.log(message.mentions.users.array())
+  //   _.each(message.mentions.users.array(), (userMention) => {
+  //     console.log("user??", userMention.id)
+  //   })
+  // }
+  _.each(config.commands, (command) => {
+    if(_.find(command.cmds, (cmd) => { return message.content == cmd})) {
+      message.channel.send(command.response)
+    }
+  })
 });
 
 client.login(auth.token);
