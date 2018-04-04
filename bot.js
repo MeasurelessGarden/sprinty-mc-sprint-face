042a6d04687d8946f13d05dad35765f1ebe0553e
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-04 00:07:38 -0600

    A really dumb command with no validation, but it will start a timer for commands like 'sprint at 5'

--- bot.js
+++ bot.js
@@ -32,12 +32,59 @@ const mentionOrDmBotCommand = (message, command) => {
   }
 }
 
+const debugMe = (debugMessage) => {
+  console.log("triggering ", debugMessage, new Date())
+}
+const textChannelMention = (message, command) => {
+  if(!command.mentionRequired) {
+    const msg = _.toLower(_.trim(message.content))
+    // console.log(msg, command)
+    const cmd = _.find(command.startsWith, (cmd)=> { return msg.startsWith(cmd) })
+    if(cmd) {
+      const args = _.split(_.trim(_.replace(msg, cmd, '')))
+      console.log(args)
+      const now = new Date()
+      // const nowMins = now.getMinutes()
+      /*
+      const startSeconds = Number(args[0])*60
+      const nowSeconds = now.getMinutes()*60 + now.getSeconds()
+      // console.log(nowMins*60+)
+      // console.log((Number(args[0])+60-nowMins)*1000)
+      if (startSeconds > nowSeconds) {
+        // console.log('start in ', start - nowMins)
+        client.setInterval(debugMe, (startSeconds > nowSeconds)*1000*60, 'testing interval')
+        client.setTimeout(debugMe, (startSeconds > nowSeconds)*1000*60, 'testing timeout')
+      } else {
+        // TODO
+      }*/
+      const target = new Date()
+      target.setMinutes(Number(args[0]))
+      target.setSeconds(0)
+      target.setMilliseconds(0)
+      timeout = target.getTime() - now.getTime()
+      console.log(now, target, now.getTime(), target.getTime(), timeout)
+      if (timeout > 0) {
+        console.log('setting a timeout in ...', timeout)
+        client.setTimeout( debugMe, timeout, 'hi')
+      } else {
+        target.setHours(target.getHours() + 1)
+        timeout = target.getTime() - now.getTime()
+        client.setTimeout( debugMe, timeout, 'wrap')
+      }
+
+
+    }
+
+  }
+}
+
 client.on("message", (message) => {
   // console.log(message)
   if (message.author.bot) {return} // prevent botception
 
   _.each(config.commands, (command) => {
     mentionOrDmBotCommand(message, command)
+    textChannelMention(message, command)
   })
 
 });
