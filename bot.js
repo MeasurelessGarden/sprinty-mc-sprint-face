c6dff1e756cbc8a6e22aa36e1b9cc4e35c1d7c0b
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-03 21:20:09 -0600

    experimenting with message format for clarity.

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 
 var auth = require('./secret.json');
+var yaml = require('js-yaml')
+var fs = require('fs')
+let test = yaml.load(fs.readFileSync('./test.yml', {encoding: 'utf-8'}))//require('./test.yml')
+console.log(test)
 
 const client = new Discord.Client();
 
@@ -9,8 +13,13 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
+  // console.log(message)
+  if (message.author.bot) {return;}
   if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+    message.channel.send("pong!\nfooo");
+  }
+  if (message.content.startsWith("help")) {
+    message.channel.send(test.cat);
   }
 });
 
