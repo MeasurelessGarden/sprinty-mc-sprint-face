9e241578058692b0946c60193c356f3ebac8e2a9
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-04 21:24:11 -0600

    Test framework - placeholder test only so far.

---
+++ test/spec.js
@@ -0,0 +1,7 @@
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+before(function () {
+  chai.use(chaiAsPromised)
+  chai.should()
+  chai.expect()
+})
---
+++ test/timeouts.js
@@ -0,0 +1,10 @@
+import './spec.js'
+
+describe('Timeouts', function(){
+
+  it('can do foo', function(){
+    const result = []
+    result.length.should.equal(0)
+  })
+
+})
