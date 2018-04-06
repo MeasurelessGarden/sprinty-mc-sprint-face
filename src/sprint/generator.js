8269314a1e2fc039ba1f629921200930f30fd459
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-05 21:16:23 -0600

    Actual implementation of sprint function plus more tests.

--- src/sprint/generator.js
+++ src/sprint/generator.js
@@ -3,35 +3,18 @@ export const generateSprintWithDuration = (now, startMin, duration) => {
   // start, a number between 0 and 59
   // duration, a number between 1 and 60
   const start = new Date(now)
+  start.setMinutes(startMin)
+  start.setSeconds(0)
+  start.setMilliseconds(0)
+  let timeout = start.getTime() - now.getTime()
+  if (timeout < 0) {
+    start.setHours(start.getHours() + 1) // TODO test over daytime boundary
+    timeout = start.getTime() - now.getTime()
+  }
+  const end = new Date(start)
+  end.setMinutes(end.getMinutes() + duration)
   return {
-    start: new Date(Date.parse('April 5, 2018 2:15 pm')),
-    end: new Date(Date.parse('April 5, 2018 2:45 pm')),
+    start: start,
+    end: end,
   }
 }
-/*
-start.setMinutes(Number(args[0]))
-start.setSeconds(0)
-start.setMilliseconds(0)
-timeout = start.getTime() - now.getTime()
-if (timeout < 0) {
-  start.setHours(start.getHours() + 1)
-  timeout = start.getTime() - now.getTime()
-}
-const defaultMinutes = 2
-end.setHours(start.getHours())
-end.setMinutes(start.getMinutes() + defaultMinutes)
-end.setSeconds(0)
-end.setMilliseconds(0)
-client.setTimeout(startSprint, timeout, start.getTime())
-client.setTimeout(
-  endSprint,
-  end.getTime() - now.getTime(),
-  start.getTime()
-)
-cache[start.getTime()] = {
-  // sprint definition
-  start: start,
-  end: end,
-  channel: message.channel,
-}
-*/
--- test/sprint/generatorSpec.js
+++ test/sprint/generatorSpec.js
@@ -9,14 +9,11 @@ const itGeneratesSprintWithDuration = (now, startMin, duration, expected) => {
 }
 
 describe('Sprint generator', function(){
-  // it('parsing dates...', function(){
-  //   console.log("foobar", new Date(Date.parse("April 12, 2019 2:00 pm")))
-  // })
   it('can run a function', function(){
     const sprint = generateSprintWithDuration(new Date(), 15, 20)
   })
   unroll(
-    'generates a sprint from #now starting at #startMin with duration #duration',
+    'generates a sprint from #now starting at #startMin with duration #duration - #reason',
     function(done, args){
       itGeneratesSprintWithDuration(
         args.now,
@@ -27,14 +24,95 @@ describe('Sprint generator', function(){
       done()
     },
     [
-      [ 'now', 'startMin', 'duration', 'expected' ],
+      [ 'reason', 'now', 'startMin', 'duration', 'expected' ],
       [
+        'simple',
         new Date(Date.parse('April 5, 2018 2:00 pm')),
         15,
         30,
         {
           start: new Date(Date.parse('April 5, 2018 2:15 pm')),
           end: new Date(Date.parse('April 5, 2018 2:45 pm')),
+        },
+      ],
+      [
+        'end wraps to next hour',
+        new Date(Date.parse('April 5, 2018 2:00 pm')),
+        45,
+        30,
+        {
+          start: new Date(Date.parse('April 5, 2018 2:45 pm')),
+          end: new Date(Date.parse('April 5, 2018 3:15 pm')),
+        },
+      ],
+      [
+        'start and end wrap to next hour',
+        new Date(Date.parse('April 5, 2018 2:15 pm')),
+        0,
+        30,
+        {
+          start: new Date(Date.parse('April 5, 2018 3:00 pm')),
+          end: new Date(Date.parse('April 5, 2018 3:30 pm')),
+        },
+      ],
+      [
+        'start wraps to next hour, end wraps to hour after *that*',
+        new Date(Date.parse('April 5, 2018 2:45 pm')),
+        40,
+        30,
+        {
+          start: new Date(Date.parse('April 5, 2018 3:40 pm')),
+          end: new Date(Date.parse('April 5, 2018 4:10 pm')),
+        },
+      ],
+      [
+        'end wraps to exactly next hour',
+        new Date(Date.parse('April 5, 2018 2:15 pm')),
+        30,
+        30,
+        {
+          start: new Date(Date.parse('April 5, 2018 2:30 pm')),
+          end: new Date(Date.parse('April 5, 2018 3:00 pm')),
+        },
+      ],
+      [
+        'end wraps to next hour offset from quarter-hours',
+        new Date(Date.parse('April 5, 2018 2:45 pm')),
+        50,
+        30,
+        {
+          start: new Date(Date.parse('April 5, 2018 2:50 pm')),
+          end: new Date(Date.parse('April 5, 2018 3:20 pm')),
+        },
+      ],
+      [
+        'end wraps to next day',
+        new Date(Date.parse('2018-04-05T23:30:00.000Z')),
+        45,
+        30,
+        {
+          start: new Date(Date.parse('2018-04-05T23:45:00.000Z')),
+          end: new Date(Date.parse('2018-04-06T00:15:00.000Z')),
+        },
+      ],
+      [
+        'start and end wrap to next day',
+        new Date(Date.parse('2018-04-05T23:45:00.000Z')),
+        15,
+        30,
+        {
+          start: new Date(Date.parse('2018-04-06T00:15:00.000Z')),
+          end: new Date(Date.parse('2018-04-06T00:45:00.000Z')),
+        },
+      ],
+      [
+        'different duration',
+        new Date(Date.parse('2018-04-05T05:10:00.000Z')),
+        15,
+        37,
+        {
+          start: new Date(Date.parse('2018-04-05T05:15:00.000Z')),
+          end: new Date(Date.parse('2018-04-05T05:52:00.000Z')),
         },
       ],
     ]
