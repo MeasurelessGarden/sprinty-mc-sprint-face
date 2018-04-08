de48407982107b9a602907019525ca2285d5be6c
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-08 13:26:44 -0600

    Cleanup my test code, run formatter.

--- src/commands/helpCommand.js
+++ src/commands/helpCommand.js
@@ -1,6 +1,4 @@
-import {
-  generateHelp,
-} from './helpUtils.js'
+import {generateHelp} from './helpUtils.js'
 
 import {sprintIntro, sprintCommands} from './sprintCommand.js'
 
@@ -14,59 +12,52 @@ Note, for spam reasons, Sprinty will only reply to you in a DM! Please DM Sprint
 Xe acknowledges commands with reactions, so that's how you'll know if Sprinty heard you!
 
 Sprinty parses many commands out of normally phrased text, so it's important to dig through the help and figure out how to say what you mean, and understand the places where xe can get it hilariously wrong.`
-// Here's some basic commands to get you started:
-//
-// - \`help\` (bet you figured out this one!)
-// - \`help sprint\` - get more info on managing sprints
-// - \`help sprint examples\` - because of the command matching, this is extensive, which is \`help sprint\` exists to just list the options consisely.
-// `
 
 const BasicHelpTemplate = {
-  exact: true, // TODO need to figure out how to test this
+  // exact: true, // TODO need to figure out how to test this - for now, it'll just depend on the same matching everything else does
   input: [],
   call: (...args) => {
-    return generateHelp(helpIntro, helpCommands) //TODO have a feeling I'm going to get into a which-is-defined-first loop :(
+    return generateHelp(helpIntro, helpCommands)
   },
   additionalHelp:
     '(bet you figured out this one!) Please always ask for help in a DM to Sprinty.',
 }
 
 const CommandHelpTemplate = {
-  exact: true, // TODO need to figure out how to test this
+  // exact: true,
   input: [
     {
       name: 'command',
       type: 'Command', // TODO validate that a command cannot use this template unless it has the right args inside it (include default param - meaning not required for this, and also has a value)
-      // units: 'minutes of hour',
-      description: 'must be one of: sprint',
+      description: 'must be one of: sprint', // TODO this has some duplication in parseUtils for anything ever to work (for now)
       checks: [ arg => arg == 'sprint' ],
     },
   ],
   call: (...args) => {
     // TODO once we have more commands, this will have to look at arg[1] (arg[0] is timestamp) and switch on which intro/commands to put into the function.
     // I guess I could test it with `help help` ....
-    console.log('wtf', sprintIntro)
     return generateHelp(sprintIntro, sprintCommands)
   },
   additionalHelp:
-    'Get more info on managing sprints.',
+    'Get more info on managing sprints. This command must be in a DM.',
 }
 
 export const helpCommands = [
-
-    {
-      vocabulary: [ 'help', 'Command' ],
-      template: CommandHelpTemplate,
-      examples: [  {
-          name: 'sprint',
-           // TODO flag these help: 'basic'?
-          input: 'help sprint',
-        },]
-    },
+  {
+    vocabulary: [ 'help', 'Command' ],
+    template: CommandHelpTemplate,
+    examples: [
+      {
+        name: 'sprint',
+        // TODO flag these help: 'basic'?
+        input: 'help sprint',
+      },
+    ],
+  },
   {
     vocabulary: [ 'help' ],
     template: BasicHelpTemplate,
     // examples: []
   },
-    //help command examples - allows 'help help examples'
+  //help command examples - allows 'help help examples'
 ]
--- src/commands/parseUtils.js
+++ src/commands/parseUtils.js
@@ -9,35 +9,28 @@ export const preparseMessage = message => {
 }
 
 // this is part of the command itself! or at least - how I structure commands TODO ... no I just need a consistent set of terms. command in this context is different than in another...
-export const parseMessageToArray = (message, command) => { // TODO rename command to vocabulary
+export const parseMessageToArray = (message, command) => {
+  // TODO rename command to vocabulary
   // assumes message is cleaned by preparse first
   // command, an array to match terms from to identify command and args
+
   const parseArgsEquality = (value, arg) => {
     if (_.isEqual(arg, 'Number')) {
       return Number(value) >= 0 // TODO note that the removal of punctuation means negative numbers cannot happen anyway
     }
     if (_.isEqual(arg, 'Command')) {
-      console.log('matching equality of Command for', value)
       // TODO validCommands should come from the template
-      const validCommands = ['sprint'] // even though I'm not strictly doing validation here, otherwise I'm overmatching weirdly (TODO might not be an issue if I make exact flag work....)
-      return _.find(validCommands, cmd => {return _.isEqual(value, cmd)}) // this doesn't do validation yet - any string looks like a valid command
+      const validCommands = [ 'sprint' ] // even though I'm not strictly doing validation here, otherwise I'm overmatching weirdly (TODO might not be an issue if I make exact flag work....)
+      return _.find(validCommands, cmd => {
+        return _.isEqual(value, cmd)
+      }) // this doesn't do validation yet - any string looks like a valid command
       // TODO although if we did validation here, it might be good....
     }
     return _.isEqual(value, arg)
   }
+
   const matchingValues = []
   let workingMessage = _.split(message, ' ') // TODO I could split terms *before* I replace all the misc characters?... just gotta throw out any empty-ish pieces after...
-  // if (isExact) {
-  //   // console.log('exact match required!', workingMessage, command, templateInputs, validate(templateInputs, parseCommandArgs(
-  //   //   command,
-  //   //   parseMessageToArgs(workingMessage, command, isExact),
-  //   //   templateInputs
-  //   // )))
-  //   return validate(templateInputs, workingMessage) ? workingMessage : []
-  //   // console.log('exact match required!', workingMessage, command, _.isEqual(workingMessage, command, parseArgsEquality))
-  //   // return _.isEqual(workingMessage, command, (a,b)=>{return !_.find(parseArgsEquality)}) ? workingMessage : []
-  // }
-  console.log('matching....', command, workingMessage)
   const partsOfMatchingCommand = _.takeWhile(command, function(param){
     if (workingMessage.length == 0) return false
 
@@ -74,7 +67,6 @@ export const convertFunctionArgsToNumbers = (message, command) => {
 
 export const parseMessageToArgs = (message, command) => {
   // puts preparse, parse, and convertFunctionArgs together
-  console.log('parsing ', message, preparseMessage(message), command)
   let m = parseMessageToArray(preparseMessage(message), command)
   if (m.length == command.length) {
     let args = convertFunctionArgsToNumbers(m, command)
@@ -105,27 +97,6 @@ const validate = (templateInputs, commandArgs) => {
   return false
 }
 
-// export const createReplyFromMessage = (commands, message) => {
-//   // message, (string) content of the message
-//   // timestamp -> pass in Message.createdTimestamp ie 1522815707792
-//   const config = _.find(commands, config => {
-//     // TODO make this use the exact flag
-//     return parseMessageToArgs(message, config.vocabulary)
-//   })
-//   if (config) {
-//     const commandArgs = parseCommandArgs(
-//       config.vocabulary,
-//       parseMessageToArgs(message, config.vocabulary),
-//       config.template.input
-//     )
-//     if (validate(config.template.input, commandArgs)) {
-//       const functionArgs = commandArgs//_.concat([ timestamp ], commandArgs)
-//       const reply = config.template.call(...functionArgs)
-//       return reply
-//     }
-//   }
-// }
-
 export const createObjFromMessage = (commands, message, timestamp) => {
   // message, (string) content of the message
   // timestamp -> pass in Message.createdTimestamp ie 1522815707792
@@ -139,12 +110,9 @@ export const createObjFromMessage = (commands, message, timestamp) => {
       config.template.input
     )
 
-    console.log('found', message, config,parseMessageToArgs(message, config.vocabulary), commandArgs)
     if (validate(config.template.input, commandArgs)) {
       const functionArgs = _.concat([ timestamp ], commandArgs)
       const obj = config.template.call(...functionArgs)
-
-      console.log('CALLING', config, config.template.call, obj)
       return obj
     }
   }
