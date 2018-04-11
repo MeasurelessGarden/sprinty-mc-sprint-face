c2cc750af6efe5c54435a8234019e58525e17eb8
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-10 21:33:39 -0600

    Migrate manual test cases into auto test cases.

--- test/commands/parseSprintCommandSpec.js
+++ test/commands/parseSprintCommandSpec.js
@@ -4,8 +4,12 @@ import {sprintCommands} from '../../src/commands/sprintCommand.js'
 import {createObjFromMessage} from '../../src/utils/parseUtils.js'
 import {expect} from 'chai'
 
-const exampleDate = (hour, min) => {
-  return new Date(Date.parse(`2018-04-07T${hour ? hour : '00'}:${min}:00.000Z`))
+const exampleDate = (hour, min, sec) => {
+  return new Date(
+    Date.parse(
+      `2018-04-07T${hour ? hour : '00'}:${min}:${sec ? sec : '00.000'}Z`
+    )
+  )
 }
 
 const expectedSprint = test => {
@@ -23,7 +27,7 @@ const unrolledExamples = _.flatMap(sprintCommands, command => {
         command,
         example.tags,
         example.input,
-        exampleDate(test.calledAtHour, test.calledAtMin),
+        exampleDate(test.calledAtHour, test.calledAtMin, test.calledAtSec),
         expectedSprint(test),
       ]
     })
@@ -48,7 +52,7 @@ const unrolledExamplesWithHeader = _.concat(
 describe('Parse Sprint Command', function(){
   describe('auto-generated tests vs single command and all sprint commands', function(){
     unroll(
-      'creates sprint from #input using #command.vocabulary, #tags',
+      'creates sprint from #input using #command.vocabulary, #tags (when called at #calledAt)',
       function(done, args){
         const sprintFromVocabulary = createObjFromMessage(
           [ args.command ],
@@ -71,163 +75,6 @@ describe('Parse Sprint Command', function(){
       unrolledExamplesWithHeader
     )
   })
-
-  unroll(
-    'generates a sprint from #message',
-    function(done, args){
-      const sprint = createObjFromMessage(
-        sprintCommands,
-        args.message,
-        1522815707792 // TODO put in example.tests as calledAtHour:... calledAtMin: ...
-      )
-      expect(sprint).to.be.equalSprintDefinition(args.expected)
-      done()
-    },
-    [
-      [ 'message', 'expected' ],
-      [
-        'ANYONE WANT TO SPRINT AT 25?', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:25:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T04:55:00.000Z')),
-        },
-      ],
-      [
-        'I want to sprint at :45', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:45:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:15:00.000Z')),
-        },
-      ],
-      [
-        "let's sprint 40 to 45", // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:40:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T04:45:00.000Z')),
-        },
-      ],
-      [
-        'sprint 15', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T05:15:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:45:00.000Z')),
-        },
-      ],
-      [
-        'sprint 15 to 25', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T05:15:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:25:00.000Z')),
-        },
-      ],
-      [
-        'sprint 15 to :35', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T05:15:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:35:00.000Z')),
-        },
-      ],
-      [
-        'sprint 57 for 32', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:57:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:29:00.000Z')),
-        },
-      ],
-      [
-        'sprint :15', //in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T05:15:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:45:00.000Z')),
-        },
-      ],
-      [
-        'sprint at 20', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T05:20:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:50:00.000Z')),
-        },
-      ],
-      [
-        'sprint at 20 for 6 min', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T05:20:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:26:00.000Z')),
-        },
-      ],
-      [
-        'sprint at 25', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:25:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T04:55:00.000Z')),
-        },
-      ],
-      [
-        'sprint at 30', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:30:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:00:00.000Z')),
-        },
-      ],
-      [
-        'sprint at 30 for 14', //in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:30:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T04:44:00.000Z')),
-        },
-      ],
-      [
-        'sprint at 35 for 14', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:35:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T04:49:00.000Z')),
-        },
-      ],
-      [
-        'sprint at 35 to 20', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:35:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:20:00.000Z')),
-        },
-      ],
-      [
-        'sprint at 55 for 55', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:55:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:50:00.000Z')),
-        },
-      ],
-      [
-        'shall we sprint :20 for about 55 min?', // in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T05:20:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T06:15:00.000Z')),
-        },
-      ],
-      [
-        'sprint 30 for 34 minutes', //in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:30:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:04:00.000Z')),
-        },
-      ],
-      [
-        'sprint at 30 for 34 minutes', //in example: check
-        {
-          start: new Date(Date.parse('2018-04-04T04:30:00.000Z')),
-          end: new Date(Date.parse('2018-04-04T05:04:00.000Z')),
-        },
-      ],
-
-      // [
-      //   'should we go at 10?',
-      //   {
-      //     start: new Date(Date.parse('2018-04-04T05:10:00.000Z')),
-      //     end: new Date(Date.parse('2018-04-04T05:40:00.000Z')),
-      //   },
-      // ],
-    ]
-  )
 
   unroll(
     'validates a sprint to control length #message - #reason',
