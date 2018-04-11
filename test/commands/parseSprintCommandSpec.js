8b39a74b4f863af722cff4da9f19c11f7d1f4b1f
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-10 21:06:28 -0600

    Clean up tmp test file - merged into parseSprintCommandSpec

--- test/commands/parseSprintCommandSpec.js
+++ test/commands/parseSprintCommandSpec.js
@@ -4,46 +4,71 @@ import {sprintCommands} from '../../src/commands/sprintCommand.js'
 import {createObjFromMessage} from '../../src/utils/parseUtils.js'
 import {expect} from 'chai'
 
-// TODO this is probably a convoluted way to create the structure I want to unroll...
-const unrollCommandExamples = _.map(
-  _.flatMap(sprintCommands, command => {
-    return _.map(command.examples, example => {
-      return {ex: example, config: command}
-    })
-  }),
-  unroll => {
-    const startHour = unroll.ex.startHour ? `${unroll.ex.startHour}` : '00'
-    const endHour = unroll.ex.endHour ? `${unroll.ex.endHour}` : '00'
-    return [
-      unroll.config,
-      unroll.ex,
-      {
-        start: new Date(
-          Date.parse(`2018-04-07T${startHour}:${unroll.ex.startMin}:00.000Z`)
-        ),
-        end: new Date(
-          Date.parse(`2018-04-07T${endHour}:${unroll.ex.endMin}:00.000Z`)
-        ),
-      },
-    ]
+const exampleDate = (hour, min) => {
+  return new Date(Date.parse(`2018-04-07T${hour}:${min}:00.000Z`))
+}
+
+const expectedSprint = test => {
+  return {
+    start: exampleDate(test.startHour ? test.startHour : '00', test.startMin),
+    end: exampleDate(test.endHour ? test.endHour : '00', test.endMin),
   }
+}
+
+const unrolledExamples = _.flatMap(sprintCommands, command => {
+  return _.flatMap(command.examples, example => {
+    return _.map(example.tests, test => {
+      return [
+        // calledAt date is the zeroth hour of the day, to make the startHour, endHour easy to predict from example definitions
+        command,
+        example.tags,
+        example.input,
+        exampleDate('00', test.calledAtMin),
+        expectedSprint(test),
+      ]
+    })
+  })
+})
+
+const unrolledExamplesWithHeader = _.concat(
+  [ [ 'command', 'tags', 'input', 'calledAt', 'expected' ] ],
+  unrolledExamples
 )
 
+// console.log('-------------------')
+// console.log('-------------------')
+// console.log('-------------------')
+// _.each(unrolledExamples, ex => {
+//   console.log(ex)
+// })
+// console.log('-------------------')
+// console.log('-------------------')
+// console.log('-------------------')
+
 describe('Parse Sprint Command', function(){
-  describe('self describing generated tests', function(){
+  describe('auto-generated tests vs single command and all sprint commands', function(){
     unroll(
-      'creates sprint from #example.input', // (#example.name) with #command.vocabulary',
+      'creates sprint from #input using #command.vocabulary, #tags',
       function(done, args){
-        const sprint = createObjFromMessage(
-          sprintCommands,
-          args.example.input,
-          1523059200000 // zero hour day, to make it easy to compute expected times (see above hacky thing to generate the unroll)
+        const sprintFromVocabulary = createObjFromMessage(
+          [ args.command ],
+          args.input,
+          args.calledAt
         )
 
-        expect(sprint).to.be.equalSprintDefinition(args.expected)
+        const sprintFromAllSprintCommands = createObjFromMessage(
+          sprintCommands,
+          args.input,
+          args.calledAt
+        )
+
+        expect(sprintFromVocabulary).to.be.equalSprintDefinition(args.expected)
+        expect(sprintFromAllSprintCommands).to.be.equalSprintDefinition(
+          args.expected
+        )
         done()
       },
-      _.concat([ [ 'command', 'example', 'expected' ] ], unrollCommandExamples)
+      unrolledExamplesWithHeader
     )
   })
 
@@ -53,7 +78,7 @@ describe('Parse Sprint Command', function(){
       const sprint = createObjFromMessage(
         sprintCommands,
         args.message,
-        1522815707792
+        1522815707792 // TODO put in example.tests as calledAtHour:... calledAtMin: ...
       )
       expect(sprint).to.be.equalSprintDefinition(args.expected)
       done()
@@ -232,7 +257,7 @@ describe('Parse Sprint Command', function(){
       ],
       [
         'sprints cannot be longer than an hour',
-        'sprint at 10 for 61',
+        'sprint at 10 for 61', // TODO add all the undefineds to 'invalid tests'
         undefined,
       ],
       [
