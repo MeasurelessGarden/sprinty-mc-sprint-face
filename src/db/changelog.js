d67e006085044d843fd63cfa19e66358ee7db346
Measureless <38166344+MeasurelessGarden@users.noreply.github.com>
2018-04-24 20:52:15 -0600

    Clean up table junk.

--- src/db/changelog.js
+++ src/db/changelog.js
@@ -3,13 +3,12 @@ import {Tables} from './Table.js'
 export const changelog = database => {
   if (database) {
     const t = new Tables(database)
-    t.listTables()
-    // t.ensureTable('guild_config', [
-    //   'guild_id              varchar(20) PRIMARY KEY',
-    //   'sprint_channel_id     varchar(20)',
-    //   'sprint_channel_date   date'])
-    t.dropTable('guild_config')
-    t.listTables()
+    t.ensureTable('guild_config', [
+      'guild_id              varchar(20) PRIMARY KEY',
+      'sprint_channel_id     varchar(20)',
+      'sprint_channel_date   date',
+    ])
+    t.listTables() // TODO no guarentee that this ran *after* the previous command finished...
   }
   else {
     console.log('sorry no db for you!')
